7c46ae6f0c72d586add36861aac9c0623c423fb3
Steven Lomax <ste.lomax@gmail.com>
2022-02-24 05:30:21 +0000

    objects up to isOver65 solved

--- src/objects.js
+++ src/objects.js
@@ -1,21 +1,23 @@
 const createPerson = (name, age) => {
-  // your code here
+  return {
+    name, age
+  }
 };
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  return object.hasOwnProperty(property);
 };
 
 const isOver65 = person => {
-  // your code here
+  return person.age > 65;
 };
 
 const getAges = people => {
